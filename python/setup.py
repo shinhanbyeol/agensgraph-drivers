0623033225d7dfb90a417b110d3ec89facadc88e
Umar Hayat <umar.hayat@bitnine.net>
2025-06-27 11:05:01 +0900

    Migrated python driver
    Migrated python driver from https://github.com/skaiworldwide-oss/agensgraph-python

---
+++ python/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agensgraph',
+    version='1.0.0',
+    description='Psycopg2 type extension module for AgensGraph',
+    install_requires=['psycopg2>=2.5.4'],
+
+    packages=find_packages(exclude=['tests']),
+    test_suite = "tests",
+
+    author='Umar Hayat',
+    author_email='skaisw@skaiworldwide.com',
+    maintainer='Umar Hayat',
+    maintainer_email='skaisw@skaiworldwide.com',
+    url='https://github.com/skaiworldwide-oss/agensgraph-drivers',
+    license='Apache License Version 2.0',
+)
