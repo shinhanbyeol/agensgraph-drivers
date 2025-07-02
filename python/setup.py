018795242034d23923eee8cc42c789d140f935c0
Umar Hayat <umar.hayat@bitnine.net>
2025-07-02 11:22:39 +0900

    Updated pypi package name and license string
    - Updated package name as previous name is already reserved in pypi registry
    - Fixed license string

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='agensgraph',
+    name='agensgraph-python',
     version='1.0.0',
     description='Psycopg2 type extension module for AgensGraph',
     install_requires=['psycopg2>=2.5.4'],
